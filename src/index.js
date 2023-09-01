92c86ce83e7850bda37a0d7d6d3e161a455981ce
cbmanu <carrero.besson@gmail.com>
2023-09-01 16:04:38 -0400

    change creation of user

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const app=require('./app')
 const database=require('./db/database')
 
 
-const PORT=process.env.PORT||3000
+const PORT=process.env.PORT||3080
 database().then(() => {
     app.listen(PORT, () => {
         console.log("listening for requests");
